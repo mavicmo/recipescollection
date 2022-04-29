973a0bf3e2721ee86fc7130010abfff781cbcd3f
mtaraq5@gmail.com <mtaraq@Mos-MacBook-Pro.local>
2022-04-29 11:13:07 -0400

    try to deploy

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,11 @@ const express = require("express");
 const methodOverride = require("method-override");
 const cors = require("cors");
 const recipesController = require("./controller/recipes-controller");
+const path = require("path");
 const app = express();
 const port = process.env.PORT || 3005;
 var distDir = __dirname + "/dist/";
+app.use(express.static(path.join(__dirname, "../frontend/index.html")));
 app.use(express.static(distDir));
 app.use(cors());
 app.use(express.json());
@@ -13,6 +15,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/recipes", recipesController);
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`Express MVC app is running on port ${port}`);
 });
